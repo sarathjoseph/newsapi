4c8b24d7ec016dbde94330e0b631004f0237ca9d
Andy Steele <andy@spacemeld.com>
2013-12-09 20:10:38 -0500

    Added in monitoring from New Relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['News API'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '44f5ee300812aa20fc2c768e354fb054659b7d0f',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
